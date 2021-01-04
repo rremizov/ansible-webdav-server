580e96473b608b0846f2e15e2764ce537d7e2024
rremizov <rremizov@yandex.ru>
2021-01-04 16:05:54 +0500

    Fix

--- molecule/default/tests/test_main.py
+++ molecule/default/tests/test_main.py
@@ -2,5 +2,5 @@ def test_nginx_is_enabled(host):
     assert host.service("nginx").is_enabled
 
 
-def test_ngix_is_running(host):
+def test_nginx_is_running(host):
     assert host.service("nginx").is_running
